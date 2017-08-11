7f5238ff512067bf36060743aff966d5d1675075
Calvin Wu <calvin620707@msn.com>
2017-08-11 11:08:16 +0800

    refine wait_for_it.py for travis

--- tests/wait_for_it.py
+++ tests/wait_for_it.py
@@ -1,18 +1,29 @@
 import sys
 import urllib.request
 import urllib.error
+from time import sleep
 
 
 def wait_for_internet_connection(host, port):
+    target = host + ':' + port
+    print("Wait for {} ready".format(target))
+    count = 0
     while True:
         try:
             urllib.request.urlopen('http://' + host + ':' + port, timeout=1)
-            print("test2")
-            return
-        except (urllib.error.URLError, urllib.error.HTTPError) as e:
+            return target
+        except Exception as e:
+            dots = ''
+            for i in range(3):
+                dots += '.' if i <= count % 3 else ' '
+            print("Still waiting for {} {}".format(target, dots), end='\r')
+            count += 1
             if type(e) is urllib.error.HTTPError:
-                return
+                return target
+            sleep(1)
             pass
 
+
 if __name__ == '__main__':
-    wait_for_internet_connection(sys.argv[1], sys.argv[2])
+    ret = wait_for_internet_connection(sys.argv[1], sys.argv[2])
+    print('\n{} is ready'.format(ret))
